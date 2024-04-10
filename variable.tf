5ddd8ed427ae79aabb2d98b8d61bb7476b9a1243
kaasu-pavani <157674223+kaasu-pavani@users.noreply.github.com>
2024-04-10 12:29:28 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,36 @@
+# Defining CIDR Block for VPC
+variable "vpc-cidr" {
+default = "10.0.0.0/16"
+}
+
+# Defining CIDR Block for 1st subnet
+Variable "subnet1-cidr" {
+default = "10.0.1.0/24"
+}
+
+# Defining CIDR Block for 2nd subnet
+variable "subnet2-cidr" {
+default = "10.0.2.0/24"
+}
+
+# Defining CIDR Block for 3rd subnet
+variable "subnet3-cidr" {
+default = "10.0.3.0/24"
+}
+
+# Defining CIDR Block for 4th subnet
+variable "subnet4-cidr" {
+default = "10.0.4.0/24"
+}
+
+# Defining CIDR Block for 5th subnet
+variable "subnet5-cidr" {
+default = "10.0.5.0/24"
+}
+
+# Defining CIDR Block for 6th subnet
+variable "subnet6-cidr" {
+default = "10.0.6.0/24"
+}
+
+
