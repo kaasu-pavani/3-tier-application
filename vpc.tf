ae6c299dd43faac574c100d8ea7e5a505d0b9299
kaasu-pavani <157674223+kaasu-pavani@users.noreply.github.com>
2024-04-15 09:46:24 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,9 @@
 # Creating vpc
-Resource "aws_vpc" "demovpc" {
-  cidr_block = "${var.vpc_cidr}"
+resource "aws_vpc" "demovpc" {
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   tags = {
     Name = "demo vpc"
   }
+
 }
